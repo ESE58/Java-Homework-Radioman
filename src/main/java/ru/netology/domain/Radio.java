d62205df2a859b2e2504368f6fb4d745647319d9
ESE58 <shepshop@ya.ru>
2021-11-09 22:26:51 +0300

    Radio.java and RadioTest.java added

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,4 +1,37 @@
 package ru.netology.domain;
 
 public class Radio {
+    public int currentStation;
+    public int currentVolume;
+
+    public void setCurrentStation(int CurrentStation) {
+        if (CurrentStation < 0) {
+            return;
+        }
+        if (CurrentStation > 9) {
+            return;
+        }
+        this.currentStation = CurrentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+        }
+        if (currentStation == 9) {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        }
+        if (currentStation == 0) {
+            currentStation = 9;
+        }
+    }
+    public int getCurrentStation() {
+        return currentStation;
+    }
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,7 +1,121 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
+    @Test
+    public void shouldCheckStationIsMinus1() {
+        Radio rd = new Radio();
+
+        rd.setCurrentStation(-1);
+
+        int expected = 0;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckStationIs0() {
+        Radio rd = new Radio();
+
+        rd.setCurrentStation(0);
+
+        int expected = 0;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckStationIs1() {
+        Radio rd = new Radio();
+
+        rd.setCurrentStation(1);
+
+        int expected = 1;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckStationIs8() {
+        Radio rd = new Radio();
+
+        rd.setCurrentStation(8);
+
+        int expected = 8;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldCheckStationIs9() {
+        Radio rd = new Radio();
+
+        rd.setCurrentStation(9);
+
+        int expected = 9;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckStationIs10() {
+        Radio rd = new Radio();
+
+        rd.setCurrentStation(10);
+
+        int expected = 0;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckNextStationCurrentIs0() {
+        Radio rd = new Radio();
+        rd.setCurrentStation(0);
+
+        rd.nextStation();
+
+        int expected = 1;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckNextStationCurrentIs9() {
+        Radio rd = new Radio();
+        rd.setCurrentStation(9);
+
+        rd.nextStation();
+
+        int expected = 0;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckPreviousStationCurrentIs0() {
+        Radio rd = new Radio();
+        rd.setCurrentStation(0);
+
+        rd.prevStation();
+
+        int expected = 9;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckPreviousStationCurrentIs9() {
+        Radio rd = new Radio();
+        rd.setCurrentStation(9);
+
+        rd.prevStation();
+
+        int expected = 8;
+        int actual = rd.getCurrentStation();
+        assertEquals(expected, actual);
+    }
 }
